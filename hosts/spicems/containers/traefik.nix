9aeb8109b1e713ca7d0701c51d9b86905cd1e6ef
eptastic <github.9uvss@aleeas.com>
2026-01-11 18:28:24 +1100

    Fixed issue with Authelia + Traefik. Was due to entrypoints.https.forwardedHeaders.trustedIPs in traefik.nix

--- hosts/spicems/containers/traefik.nix
+++ hosts/spicems/containers/traefik.nix
@@ -24,8 +24,7 @@ in {
         "--entrypoints.https.address=:443"
 
         #"--entrypoints.https.forwardedHeaders.trustedIPs=${trustedIpsString}"
-        "--entrypoints.https.forwardedHeaders.trustedIPs=127.0.0.1/32,10.0.0.0/8"
-
+        "--entrypoints.https.forwardedHeaders.trustedIPs=127.0.0.1/32,10.0.0.0/8,192.168.0.0/16,172.16.0.0/16,173.245.48.0/20,103.21.244.0/22,103.22.200.0/22,103.31.4.0/22,108.162.192.0/18,190.93.240.0/20,188.114.96.0/20,197.234.240.0/22,198.41.128.0/17,162.158.0.0/15,104.16.0.0/12,172.64.0.0/13,131.0.72.0/22,141.101.64.0/18,104.16.0.0/13,104.24.0.0/14"
         "--entrypoints.traefik.address=:8282"
         "--api=true"
         "--api.insecure=true"
